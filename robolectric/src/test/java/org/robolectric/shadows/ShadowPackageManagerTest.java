420d142ef21137284c057ddbcca358c8fb99d09e
Christian Williams <christianw@google.com>
2017-05-16 15:25:21 -0700

    Fix PackageManager#resolveActivity() and activity metadata.
    Switched DefaultPackageManager#queryIntentImplicitly to default to true,
    	so PackageManager#resolveActivity() now matches more intents.
    Activity metadata should be resolved against resources.

--- robolectric/src/test/java/org/robolectric/shadows/ShadowPackageManagerTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowPackageManagerTest.java
@@ -779,7 +779,7 @@ public class ShadowPackageManagerTest {
 
     metaValue = meta.get("org.robolectric.metaColor");
     assertTrue(Integer.class.isInstance(metaValue));
-    assertEquals(Color.WHITE, metaValue);
+    assertEquals(0xFFABCDEF, metaValue);
 
     metaValue = meta.get("org.robolectric.metaBooleanFromRes");
     assertTrue(Boolean.class.isInstance(metaValue));
@@ -804,6 +804,10 @@ public class ShadowPackageManagerTest {
     metaValue = meta.get("org.robolectric.metaStringRes");
     assertTrue(Integer.class.isInstance(metaValue));
     assertEquals(R.string.app_name, metaValue);
+
+    metaValue = meta.get("org.robolectric.metaColorRes");
+    assertTrue(Integer.class.isInstance(metaValue));
+    assertEquals(R.color.clear, metaValue);
   }
 
   @Test
